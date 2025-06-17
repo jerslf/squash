cc8668b329e208623bfa543f7bbc8878809133bf
jerslf <jerslf@hotmail.co.uk>
2025-06-17 16:16:14 +0200

    Add classes and scoreboard

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.games_won = 0
+    
+    def reset_score(self):
+        self.score = 0
+
+
+class Game:
+    def __init__(self):
+        pass
+
+
+class Match:
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,17 @@
+from classes import Player, Game, Match
+from scoreboard import display_scoreboard
+
+
+def create_players():
+    player1 = Player("Jeremy")
+    player2 = Player("Alex")
+    return player1, player2
+
+
+def main():
+    player1, player2 = create_players()
+    display_scoreboard(player1, player2)
+
+
+if __name__ == "__main__":
+    main()
