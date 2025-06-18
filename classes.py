3aec1c8d23bebb939da46ff5ae6abaa5d8c656cc
jerslf <jerslf@hotmail.co.uk>
2025-06-18 21:52:41 +0200

    Update Game Class

--- classes.py
+++ classes.py
@@ -6,13 +6,40 @@ class Player:
     
     def reset_score(self):
         self.score = 0
+    
+    def add_point(self):
+        self.score += 1
+    
+    def remove_point(self):
+        self.score -= 1
+    
+    def won_game(self):
+        self.games_won += 1
 
 
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.max_score = 11
+        self.current_game_score = (0, 0)
+
+    def update_current_game_score(self):
+        self.current_game_score  = (self.player1.score, self.player2.score)   
+
+    def point_won_by(self, player):
+        # Add point to player 1 or 2
+        if player == self.player1:
+            self.player1.add_point()
+        elif player == self.player2:
+            self.player2.add_point()
+        # Update current_game_score
+        self.update_current_game_score()
+
+    
+    
 
 
 class Match:
-    def __init__(self):
+    def __init__(self, player1, player2, games_to_win):
         pass
--- scoreboard.py
+++
@@ -1,6 +0,0 @@
-def display_scoreboard(player1, player2):
-    print(f"\n{'SCOREBOARD':^30}")
-    print("-" * 30)
-    print(f"{player1.name:<15}{player1.score:>5}")
-    print(f"{player2.name:<15}{player2.score:>5}")
-    print("-" * 30 + "\n")
